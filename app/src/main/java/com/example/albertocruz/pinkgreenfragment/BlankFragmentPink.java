115aeb37589d5d1b7fcb5a5951fc6caae651378c
Albrtx <beto_ahm@hotmail.com>
2016-10-26 16:02:38 -0400

    Landscape portrait

---
+++ app/src/main/java/com/example/albertocruz/pinkgreenfragment/BlankFragmentPink.java
@@ -0,0 +1,29 @@
+package com.example.albertocruz.pinkgreenfragment;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlankFragmentPink extends Fragment {
+
+
+    public BlankFragmentPink() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_blank_fragment_pink, container, false);
+    }
+
+}
